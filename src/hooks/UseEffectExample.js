c3b7885419fa425625ca7565667bf4cb6d7c6d68
Jonathan Schwartz <jonathanschwartz30@gmail.com>
2019-02-13 00:21:27 -0500

    Fixed static innerHeight bug in UseEffectExample (#1)

--- src/hooks/UseEffectExample.js
+++ src/hooks/UseEffectExample.js
@@ -73,7 +73,7 @@ export function Title() {
   });
   const [height, setHeight] = useState(window.innerHeight);
   useEffect(() => {
-    const handleResize = setHeight(window.innerHeight);
+    const handleResize = () => setHeight(window.innerHeight);
     window.addEventListener('resize', handleResize);
     return () => {
       window.removeEventListener('resize', handleResize);
